2a66294b0183413a71571eb397e1098d4bd894a4
Bimal Saggu <bimalsaggu28@gmail..com>
2024-04-10 20:22:46 +0530

    Build NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import '../index.css';
+import Logo from '../assets/logo.svg';
+import Cart from '../assets/cart.svg';
+import Menu from '../assets/menu.svg';
+
+const NavBar = () => {
+  return (
+    <div className="nav">
+        <div className="nav-sec">
+            <div className="nav-menu">
+                <a href="#">ALL UNIFORMS</a>
+                <a href="#">SHIRTS</a>
+                <a href="#">PANTS</a>
+                <a href="#">TIES</a>
+                <a href="#">APRONS</a>
+            </div>
+            <div className="logo">
+                <img src={Logo} alt="DressCode logo" />
+            </div>
+            <div className="cart-menu">
+                <div className="cart">
+                    <div>
+                        <img src={Cart} alt="Cart" />
+                    </div>
+                    <div className="cart-braces"><span>(0)</span></div>
+                </div>
+                <div className="ham-menu">
+                    <img src={Menu} alt="Hamburger menu" />
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default NavBar;
