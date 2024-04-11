3c184512255267b239b8138b79baab41ecfdddd0
Bimal Saggu <bimalsaggu28@gmail..com>
2024-04-11 17:58:58 +0530

    Build footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Footer from "./components/Footer"
 import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
+      <Footer />
     </>
   )
 }
