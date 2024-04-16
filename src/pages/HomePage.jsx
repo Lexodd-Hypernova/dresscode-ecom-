a8eea884d8c4cbc853f9bb0db48c08b23790bd90
Bimal Saggu <bimalsaggu28@gmail..com>
2024-04-16 18:40:13 +0530

    Sort selectedCategory issue in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,16 @@ import Footer from "../components/Footer"
 import HeadSection from "../components/HeadSection"
 import OurProducts from "../components/OurProducts"
 import NavBar from "../components/NavBar"
+import { useContext, useEffect } from "react"
+import sharedContext from "../context/SharedContext"
 
 function HomePage() {
+    const {setSelectedCategory} = useContext(sharedContext);
+
+    useEffect(() => {
+        // Reset selectedCategory to default when HomePage mounts
+        setSelectedCategory("All Uniforms");
+    }, [setSelectedCategory])
 
   return (
     <>
