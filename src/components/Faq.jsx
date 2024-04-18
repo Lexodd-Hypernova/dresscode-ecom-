6f4083a161f08685f85e990371b9b371dc4d2c70
Bimal Saggu <bimalsaggu28@gmail..com>
2024-04-18 19:05:16 +0530

    Update Faq component

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./productDetails.css";
+import Drop from "../assets/drop.svg"
 
 const Faq = () => {
   return (
@@ -7,15 +8,15 @@ const Faq = () => {
         <div className="faq-sec">
             <div className="ques">
                 <p>Lorem ipsum dolor sit amet.</p>
-                <img src="" alt="Open dropdown" />
+                <img src={Drop} alt="Open dropdown" />
             </div>
             <div className="ques">
                 <p>Lorem ipsum dolor sit amet.</p>
-                <img src="" alt="Open dropdown" />
+                <img src={Drop} alt="Open dropdown" />
             </div>
             <div className="ques">
                 <p>Lorem ipsum dolor sit amet.</p>
-                <img src="" alt="Open dropdown" />
+                <img src={Drop} alt="Open dropdown" />
             </div>
         </div>
     </div>
