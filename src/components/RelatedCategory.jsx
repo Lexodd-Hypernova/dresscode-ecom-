7d1beb7a2c3206d8294fa831bd2fcd297ac71717
Bimal Saggu <bimalsaggu28@gmail..com>
2024-04-20 17:05:10 +0530

    Add ProductDetailsPage components

---
+++ src/components/RelatedCategory.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const RelatedCategory = () => {
+  return (
+    <div>
+        <div className="rel-cat">
+            <div className="rel-head">
+                <p>Related</p>
+                <p>Categories</p>
+            </div>
+            <div className=""></div>
+        </div>
+    </div>
+  );
+};
+
+export default RelatedCategory;
--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import ProductDetails from "../components/ProductDetails";
+import RelatedCategory from "../components/RelatedCategory";
+import OurProducts from "../components/OurProducts";
+import Footer from "../components/Footer";
+import SatCus from "../assets/satCus.png";
 
 const ProductDetailsPage = () => {
   return (
     <>
         <NavBar />
         <ProductDetails  />
-        <div>ProductDetailsPage</div>
+        <RelatedCategory />
+        <OurProducts />
+        <div>
+          <div className="sat-cus">
+            <div className="sat-head">
+              <p>100% SATISFIED</p>
+              <p>CUSTOMERS</p>
+            </div>
+            <div className="sat-img">
+              <img src={SatCus} alt="Satisfied customers" />
+            </div>
+          </div>
+        </div>
+        <Footer />
     </>
   );
 };
