95fb3f015f3bcc3dc146f54d362150690a8d11af
abdulchaman <abdulchaman123@gmail.com>
2024-06-18 21:43:28 +0530

    selectByGender component done

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ import ProductPage from "./pages/ProductPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
 import Home from "./components/Home/Home";
 import SelectSchool from "./components/SelectSchool/SelectSchool";
+import SelectByGender from "./components/selectGender/SelectByGender";
 
 const router = createBrowserRouter([
     {
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
         children: [
             { index: true, element: <Home /> },
             { path: "/selectSchool", element: <SelectSchool /> },
+            { path: "/selectGender", element: <SelectByGender /> },
             { path: "/:category", element: <ProductPage /> },
             { path: "/productPage/:productId/:productName", element: <ProductDetailsPage /> },
         ]
