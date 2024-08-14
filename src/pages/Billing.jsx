69e270fa2e359be177e2cdd3cb012b3d825c2e3a
Sarik Anwar <sarikanwar007@gmail.com>
2024-08-14 21:46:23 +0530

    removed *100

--- src/pages/Billing.jsx
+++ src/pages/Billing.jsx
@@ -108,7 +108,7 @@ const Billing = () => {
 
   const handlePayment = async () => {
     try {
-      const amountInPaise = TotalPriceAfterDiscount * 100; // Example amount in paise (i.e., 1000 paise = 10 INR)
+      const amountInPaise = TotalPriceAfterDiscount ; // Example amount in paise (i.e., 1000 paise = 10 INR)
 
       if (amountInPaise < 100) {
         alert("Amount should be at least 100 paise.");
