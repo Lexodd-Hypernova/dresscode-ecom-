886719a249d4516a40c37fd315c871259015af2a
anurag <anuragchachan97@gmail.com>
2024-08-17 17:51:05 +0530

    invoice generation dynamic data

--- src/pages/PaymentSuccess.jsx
+++ src/pages/PaymentSuccess.jsx
@@ -1,15 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import InvoiceDownload from "../components/InvoiceDownload";
+
 import axios from "axios";
+import InvoiceGenerate from "../components/InvoiceGenerate";
 
-const PaymentSuccess = ({ orderID }) => {
-  const BaseURL = "https://dresscode-test.onrender.com";
-  orderID = "23C1F3";
+const PaymentSuccess = ({ orderID = "86CD35" }) => {
+  const BaseURL = "https://dresscode-updated.onrender.com";
+  const [orderDetails, setOrderDetails] = useState({});
 
-  // useEffect(() => {
-  //   getOrderDetails();
-  // }, []);
+  useEffect(() => {
+    getOrderDetails();
+  }, []);
 
   //API to get order details using order id
   const getOrderDetails = async () => {
@@ -22,55 +23,16 @@ const PaymentSuccess = ({ orderID }) => {
     };
 
     try {
-      //add get order by order id path
-      const response = await axios.get(``, config);
-      console.log(response, "25");
-      return response.data;
+      const response = await axios.get(
+        `${BaseURL}/dashboard/getOrderDetails/${orderID}`,
+        config
+      );
+      setOrderDetails(response.data.orderDetails);
     } catch (error) {
       console.error("Error fetching account info:", error);
     }
   };
 
-  const orderDetails = {
-    invoiceNumber: "123456",
-    date: "2024-08-14",
-    soldBy: {
-      name: "Seller Name",
-      address: "123 Seller St",
-      city: "Seller City",
-      state: "Seller State",
-      zip: "12345",
-      pan: "AAKL",
-      gst: "er3536dbdcjh73",
-      orderNumber: orderID,
-      orderDate: "20-07-2024",
-    },
-    billingAddress: {
-      name: "Buyer Name",
-      address: "456 Buyer Ave",
-      city: "Buyer City",
-      state: "Buyer State",
-      zip: "67890",
-    },
-    items: [
-      {
-        description: "Item 1",
-        unitPrice: 10.0,
-        discount: 1.0,
-        quantity: 2,
-        netAmt: 18.0,
-        totalAmt: 20.0,
-      },
-      {
-        description: "Item 2",
-        unitPrice: 20.0,
-        discount: 2.0,
-        quantity: 1,
-        netAmt: 18.0,
-        totalAmt: 20.0,
-      },
-    ],
-  };
   return (
     <div className="w-100vw h-100vh d-flex flex-column justify-content-center align-items-center">
       <h1 className="fs-2 mt-5 mb-5 text-center">
@@ -91,12 +53,9 @@ const PaymentSuccess = ({ orderID }) => {
         >
           Home page
         </Link>
-        <div
-          className="btn fs-4 text-white"
-          style={{ backgroundColor: "#F47458" }}
-        >
-          <InvoiceDownload data={orderDetails} />
-        </div>
+        {Object.keys(orderDetails).length > 0 && (
+          <InvoiceGenerate data={orderDetails} />
+        )}
       </div>
     </div>
   );
