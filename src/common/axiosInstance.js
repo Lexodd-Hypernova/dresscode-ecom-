46936f2b0206ab16db4dd57b86f24e474df018cd
DevTeam-Lexodd <devteam@lexodd.com>
2024-10-08 17:39:20 +0545

    Update axiosInstance.js

--- src/common/axiosInstance.js
+++ src/common/axiosInstance.js
@@ -4,8 +4,8 @@ import { useNavigate } from "react-router-dom";
 
 
 const axiosInstance = axios.create({
-    baseURL: 'https://dresscode-updated.onrender.com',
-    // baseURL: 'https://dresscode-bck.onrender.com',
+    // baseURL: 'https://dresscode-updated.onrender.com',
+    baseURL: 'https://dresscode-bck.onrender.com',
 
     withCredentials: true, // Ensures cookies (refresh token) are sent with requests
 });
